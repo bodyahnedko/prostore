3f990f14a10c3a1e4cb85f1bd66e2ee4c3a67ecd
Bodya <bodyahnedko@gmail.com>
2025-05-01 14:26:44 +0300

    Added header and footer; env variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/app/assets/styles/globals.css';
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
+import { ThemeProvider } from 'next-themes';
 
 const fontInter = Inter({
   variable: '--font-inter',
@@ -8,8 +10,12 @@ const fontInter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: 'Prostore',
-  description: 'NextJs app',
+  title: {
+    template: `%s | Prostore`,
+    default: APP_NAME,
+  },
+  description: APP_DESCRIPTION,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
@@ -18,8 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${fontInter.variable} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${fontInter.variable} antialiased`}>
+        <ThemeProvider attribute={'class'} defaultTheme={'light'} enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
