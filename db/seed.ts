8286f11c01b7a7a37d51002fdbe8e87c16f01ce0
Bodya <bodyahnedko@gmail.com>
2025-05-06 21:36:34 +0300

    seed users; auth init

--- db/seed.ts
+++ db/seed.ts
@@ -5,6 +5,13 @@ async function main() {
   await prisma.product.deleteMany();
   await prisma.product.createMany({ data: sampleData.products });
 
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.user.deleteMany();
+
+  await prisma.user.createMany({ data: sampleData.users });
+
   console.log('Database seeded successfully!');
 }
 
